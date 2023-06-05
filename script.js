5a96212a93d9d201f49ee5c7c17da832bcba2b4e
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-06-05 14:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+	event.preventDefault();
+	const radius =document.getElementById("radius").value
+const volume =(4/3 *Math.PI * radius ** 3);
+document.getElementById("volume").value= volume.tofixed(4);
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
